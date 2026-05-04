c0c2a64238e5b3f35784ecc67b9c8f37703f747b
Saintles <juls.kovalenko@gmail.com>
2026-05-04 21:26:15 +0600

    feat: implement towelSort algorithm for matrix zigzag flattening

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function towelSort(/* matrix */) {
-  throw new Error('Not implemented');
+module.exports = function towelSort(matrix) {
+  if (!matrix || !matrix.length) return [];
+
+  return matrix.reduce((result, row, index) => {
+    if (index % 2 === 0) {
+      return result.concat(row);
+    }
+    return result.concat(row.slice().reverse());
+  }, []);
 };
